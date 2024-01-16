b4b45bf7eec208434add41400419e742f6423668
BtsuanEyakem <btsuanme@gmail.com>
2024-01-15 22:28:43 -0600

    changes

--- src/MainTest.java
+++ src/MainTest.java
@@ -4,6 +4,7 @@ class MainTest {
         String name = "Hi\uD83D\uDC4B, my name is \u1265\u1341\u12A0\u1295 \u12A5\u12EB\u1244\u121D";
         //my name using Amharic alphabet is
         System.out.print(name);
+        System.out.println("cehcke");
         //to find size each code and each unicode is counted
         // the wave symbole has a unocodes that is described by two words so they each count separatley
         System.out.println("The total size of the above sentence is: "+ name.length());
